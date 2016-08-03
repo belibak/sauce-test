d50371280d3da10dccfb2c4acca77fdb7f9061fd
Alexander Rusetski <alex.rus@core-tech.ru>
2016-08-03 14:13:24 +0300

    try 1

--- devpanelcopy.py
+++ devpanelcopy.py
@@ -15,9 +15,6 @@ driver = webdriver.Remote(
    command_executor='http://fiovit:9418f0f6-032b-4234-af8a-0ddc896eb84e@ondemand.saucelabs.com:80/wd/hub',
    desired_capabilities=desired_cap)
 
-
-#driver = webdriver.Firefox()
-
 driver.implicitly_wait(10)
 driver.get("https://uiv2.devpanel.com/")
 if not "DevPanel" in driver.title:
